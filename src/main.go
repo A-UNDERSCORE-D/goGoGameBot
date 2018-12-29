3418ac0ee1a7c71431bc94216bce7971a8d75df5
A_D <A_D@snoonet.org>
2018-12-29 04:45:44 +0200

    updated main to use a manager

--- src/main.go
+++ src/main.go
@@ -1,74 +1,34 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
     "github.com/A-UNDERSCORE-D/goGoGameBot/src/process"
     "github.com/chzyer/readline"
     "log"
-    "sync"
     "time"
 )
 
-func init() {
-    log.SetFlags(log.LstdFlags)
+var rl *readline.Instance
 
+func init() {
+    log.SetFlags(/*log.LstdFlags*/0)
+    lrl, err := readline.New("> ")
+    if err != nil {
+        panic(err)
+    }
+    rl = lrl
+    log.SetOutput(rl)
 }
 
 func main() {
-    testProc, err := process.NewProcess("test", "/usr/bin/ncat", []string{"127.0.0.1", "1337"})
-    if err != nil {
-        panic(err)
-    }
+    // This breaks occasionally. Its due to the time it takes for the kernel to allocate the socket for ncat etc
+    // Its not an issue with the code AFAIK, the sleep in the start tends to fix it
+    man := process.NewManager(
+        rl,
+        process.NewProcessMustSucceed("listener", "/usr/bin/ncat", []string{"127.0.0.1", "1337", "--listen"}, rl),
+        process.NewProcessMustSucceed("client", "/usr/bin/ncat", []string{"127.0.0.1", "1337"}, rl),
+    )
 
-    rl, err := readline.New("> ")
-    defer rl.Close()
-
-    log.SetOutput(rl)
-    wg := sync.WaitGroup{}
-    wg.Add(1)
-    go func() {
-        for {
-            line, err := rl.Readline()
-            if err != nil {
-                break
-            }
-            if line == "quit" {
-                wg.Done()
-                return
-            }
-            _, err = rl.Write([]byte(line))
-            if err != nil {
-                break
-            }
-        }
-    }()
-
-    go func() {
-        s := bufio.NewScanner(testProc.Stdout)
-        for s.Scan() {
-            fmt.Println(s.Text())
-        }
-        fmt.Println("DONE")
-    }()
-
-    err = testProc.Start()
-    if err != nil {
-        panic(err)
-    }
-
-    _, err = testProc.Write("Here has a test message :D")
-    if err != nil {
-        panic(err)
-    }
-
-    go func() {
-        for {
-            select {
-            case <- time.Tick(time.Second):
-                log.Print(testProc.Status)
-            }
-        }
-    }()
-    wg.Wait()
+    man.StartAllProcessesDelay(time.Millisecond * 10)
+    man.WriteToProcess("client", "test!")
+    time.Sleep(time.Minute)
 }
