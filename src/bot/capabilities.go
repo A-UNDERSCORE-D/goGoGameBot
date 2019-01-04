de7f7d604accd4413d886d44c318c89990e3ffc9
A_D <A_D@snoonet.org>
2019-01-04 18:34:09 +0200

    refactored capability manager

--- src/bot/capabilities.go
+++ src/bot/capabilities.go
@@ -2,85 +2,157 @@ package bot
 
 import (
     "fmt"
-    "github.com/A-UNDERSCORE-D/goGoGameBot/src/util"
+    "git.fericyanide.solutions/A_D/goGoGameBot/src/util"
+    "github.com/goshuirc/irc-go/ircmsg"
     "strings"
+    "sync"
 )
 
+type Capability struct {
+    Name      string
+    Params    string
+    Callback  func(*Capability, ircmsg.IrcMessage, *sync.WaitGroup)
+    requested bool
+    available bool
+    enabled   bool
+}
+
+func (c *Capability) String() string {
+    return fmt.Sprintf("%s: %s", c.Name, c.Params)
+}
+
 type CapabilityManager struct {
-    capsToRequest []string
-    capsAvailable []string
-    capsAccepted  []string
-    bot           *Bot
+    capabilities []*Capability
+    bot          *Bot
 }
 
-func (c *CapabilityManager) requestCap(requestedCap string) {
-    c.capsToRequest = append(c.capsToRequest, requestedCap)
+func (cm *CapabilityManager) requestCap(capability *Capability) {
+    capability.requested = true
+    cm.capabilities = append(cm.capabilities, capability)
 }
 
-func (c *CapabilityManager) hasCap(requestedCap string) bool {
-    for _, v := range c.capsAccepted {
-        capName := v
-        if strings.Contains(capName, "=") {
-            capName = strings.Split(capName, "=")[0]
+func (cm *CapabilityManager) getCapByName(name string) *Capability {
+    for _, ca := range cm.capabilities {
+        if ca.Name == name {
+            return ca
         }
+    }
+    return nil
+}
 
-        if requestedCap == v {
-            return true
-        }
+func (cm *CapabilityManager) hasCap(requestedCap string) bool {
+    if c := cm.getCapByName(requestedCap); c != nil {
+        return c.available
     }
     return false
 }
 
-func (c *CapabilityManager) NegotiateCaps() {
+func (cm *CapabilityManager) filterCaps(f func(*Capability) bool) []*Capability {
+    var out []*Capability
+    for _, c := range cm.capabilities {
+        if f(c) {
+            out = append(out, c)
+        }
+    }
+    return out
+}
 
-    if len(c.capsToRequest) < 1 {
+func (cm *CapabilityManager) NegotiateCaps() {
+
+    if len(cm.capabilities) < 1 {
         return
     }
 
-    lineChan, done := c.bot.GetRawChan("CAP")
-    if err := c.bot.WriteLine(util.MakeSimpleIRCLine("CAP", "LS", "302")); err != nil {
-        c.bot.Error(fmt.Errorf("could not negotiate capabilities: %s", err))
+    lineChan, done := cm.bot.GetRawChan("CAP")
+    if err := cm.bot.WriteLine(util.MakeSimpleIRCLine("CAP", "LS", "302")); err != nil {
+        cm.bot.Error(fmt.Errorf("could not negotiate capabilities: %s", err))
         return
     }
-    defer func() { done <- true }()
-    defer func() { c.bot.WriteLine(util.MakeSimpleIRCLine("CAP", "END")) }()
+    wg := sync.WaitGroup{}
+    wg.Add(1)
+    // TODO(A_D): This is an ugly as hell way of doing this. there has to be a better way
+    firstRun := true
+    go func() {
+        // Allowing other things to run, then close up and send cap end
+        wg.Wait()
+        close(done)
+        cm.bot.WriteLine(util.MakeSimpleIRCLine("CAP", "END"))
+    }()
 
-
-
-    loop:
     for line := range lineChan {
         switch line.Params[1] {
         case "LS":
-            if line.Params[2] == "*" {
-                // This is multiline, lets just append for now
-                c.capsAvailable = append(c.capsAvailable, strings.Split(line.Params[3], " ")...)
-                continue
+            cm.addAvailableCapability(line)
+            if !firstRun {
+                wg.Add(1)
             } else {
-                c.capsAvailable = append(c.capsAvailable, strings.Split(line.Params[2], " ")...)
+                firstRun = false
             }
-            c.bot.Log.Printf("Server offered caps: %s", strings.Join(c.capsAvailable, " ,"))
+            if line.Params[2] == "*" {
+                continue // This is multiline, lets just append for now
+            }
 
-            if err := c.requestCaps(); err != nil {
-                c.bot.Error(fmt.Errorf("could not request caps: %s", err))
+            cm.bot.Log.Printf("Server offered caps: %v",
+                cm.filterCaps(func(capability *Capability) bool { return capability.available }),
+            )
+
+            if err := cm.requestCaps(); err != nil {
+                cm.bot.Error(fmt.Errorf("could not request caps: %s", err))
                 return
             }
 
         case "ACK":
-            c.capsAccepted = append(c.capsAccepted, strings.Split(line.Params[1], " ")...)
-            break loop
+            for _, v := range strings.Split(line.Params[2], " ") {
+                name, params := parseCapStr(v)
+                if c := cm.getCapByName(name); c != nil {
+                    c.enabled = true
+                    if c.Callback != nil {
+                        wg.Add(1)
+                        println("calling func")
+                        go c.Callback(c, line, &wg)
+                    }
+                } else {
+                    cm.bot.Log.Printf(
+                        "[WARN] Got an ACK for a cap we dont know about: %q with params %q", name, params,
+                    )
+                }
+            }
+            wg.Done()
 
         case "NAK":
             // To be implemented >.>
             panic("Not Implemented")
         }
     }
-
 }
 
-func (c *CapabilityManager) requestCaps() error {
-    capsToReq := capIntersect(c.capsAvailable, c.capsToRequest)
-    err := c.bot.WriteLine(
-        util.MakeSimpleIRCLine("CAP", "REQ", strings.Join(capsToReq, " ")),
+func (cm *CapabilityManager) addAvailableCapability(line ircmsg.IrcMessage) {
+    for _, capStr := range strings.Split(line.Params[len(line.Params) - 1], " ") {
+        name, params := parseCapStr(capStr)
+        if c := cm.getCapByName(name); c != nil {
+            c.available = true
+            c.Params = params
+            cm.bot.Log.Printf("%#v", c)
+        } else {
+            cm.capabilities = append(
+                cm.capabilities,
+                &Capability{Name: name, Params: params, requested: false, available: true},
+            )
+        }
+    }
+}
+
+func (cm *CapabilityManager) requestCaps() error {
+    capsToReq := cm.filterCaps(func(capability *Capability) bool {
+        return capability.requested && capability.available
+    })
+
+    var caps []string
+    for _, c := range capsToReq {
+        caps = append(caps, c.Name)
+    }
+    err := cm.bot.WriteLine(
+        util.MakeSimpleIRCLine("CAP", "REQ", strings.Join(caps, " ")),
     )
     if err != nil {
         return err
@@ -88,14 +160,10 @@ func (c *CapabilityManager) requestCaps() error {
     return nil
 }
 
-func capIntersect(s1, s2 []string) []string {
-    var out []string
-    for _, v1 := range s1 {
-        for _, v2 := range s2 {
-            if v1 == v2 {
-                out = append(out, v1)
-            }
-        }
+func parseCapStr(capStr string) (string, string) {
+    split := strings.SplitN(capStr, "=", 1)
+    if len(split) < 2 {
+        split = append(split, "")
     }
-    return out
+    return split[0], split[1]
 }
