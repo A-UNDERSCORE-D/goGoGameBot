112aeb70747b5219fb38ccad8772bf8b6f96bb06
A_D <A_D@snoonet.org>
2019-01-05 09:05:09 +0200

    made command line interaction work

--- src/cli/interaction.go
+++ src/cli/interaction.go
@@ -1,27 +1,36 @@
 package cli
 
 import (
+    "git.fericyanide.solutions/A_D/goGoGameBot/src/bot"
     "github.com/chzyer/readline"
+    "strings"
 )
 
 var rl *readline.Instance
 
-func InitCLI(Lrl *readline.Instance) {
+func InitCLI(Lrl *readline.Instance, b *bot.Bot) {
     rl = Lrl
-    go runCLI()
+    go runCLI(b)
 }
 
-func runCLI() {
+func runCLI(b *bot.Bot) {
     for {
         line, err := rl.Readline()
         if err != nil {
             // We're at an EOF, quit out
             return
         }
-        handleLine(line)
+        handleLine(line, b)
     }
 }
 
-func handleLine(line string) {
-    //split := strings.Split(line, " ")
+func handleLine(line string, b *bot.Bot) {
+
+    splitLine := strings.Split(line, " ")
+
+    b.CmdHandler.FireCommand(bot.CommandData{
+        Command:splitLine[0],
+        Args: splitLine[1:],
+        IsFromIRC: false,
+    })
 }
