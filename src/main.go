fe98edbc1b242a35dad75077ef2fedad60b0f595
A_D <A_D@snoonet.org>
2019-01-05 09:14:59 +0200

    moved interaction to main

--- src/main.go
+++ src/main.go
@@ -1,45 +1,43 @@
 package main
 
 import (
+    "fmt"
     "git.fericyanide.solutions/A_D/goGoGameBot/src/bot"
-    "git.fericyanide.solutions/A_D/goGoGameBot/src/cli"
     "git.fericyanide.solutions/A_D/goGoGameBot/src/config"
     "github.com/chzyer/readline"
     "log"
+    "strings"
 )
 
-var rl *readline.Instance
-
-func init() {
-    log.SetFlags( /*log.LstdFlags*/ 0)
-    lrl, err := readline.New("> ")
-    if err != nil {
-        panic(err)
-    }
-    rl = lrl
-    log.SetOutput(rl)
-    cli.InitCLI(rl)
-
-}
-
 func main() {
-    rl, err := readline.New("> ")
-    if err != nil {
-        panic(err)
-    }
+    rl, _ := readline.New("> ")
+    log.SetFlags(0)
+    log.SetOutput(rl)
     defer rl.Close()
+
     conf, err := config.GetConfig("config.xml")
     if err != nil {
         panic(err)
     }
-    log.Print(conf)
 
     b := bot.NewBot(*conf, log.New(rl, "[bot] ", 0))
+    go func() {
+        for {
+            line, err := rl.Readline()
+            if err != nil {
+                // We're at an EOF, quit out
+                return
+            }
+            splitLine := strings.Split(line, " ")
 
-    ch := bot.NewCommandHandler(b, "~")
-    ch.RegisterCommand("test", func(data bot.CommandData) error {
-        _, err := rl.Write([]byte("test"))
-        return err
-    }, bot.PriNorm)
-    panic(b.Run())
+            b.CmdHandler.FireCommand(bot.CommandData{
+                Command:   splitLine[0],
+                Args:      splitLine[1:],
+                IsFromIRC: false,
+            })
+        }
+    }()
+
+    b.Run()
+    fmt.Println("done")
 }
