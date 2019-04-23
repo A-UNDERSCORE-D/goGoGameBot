ec13d862aeaf11033d97971b72350772e76293b5
A_D <A_D@snoonet.org>
2019-04-23 06:58:40 +0200

    added doccumentation to event.go

--- pkg/event/event.go
+++ pkg/event/event.go
@@ -6,8 +6,10 @@ import (
 	"time"
 )
 
-// This is a reimplementation of eventmgr found at github.com/goshuirc/eventmgr with an ID system added. The original idea is theirs.
+// This is a reimplementation of eventmgr found at github.com/goshuirc/eventmgr with an ID system added.
+// The original idea is theirs.
 
+// HandlerList is a slice of handlers with functions added to allow the slice to be sorted
 type HandlerList []Handler
 
 func (h HandlerList) Len() int {
@@ -22,21 +24,27 @@ func (h HandlerList) Swap(i, j int) {
 	h[i], h[j] = h[j], h[i]
 }
 
+// Map is a map of string to HandlerList, it exists as a type alias for ease of use
 type Map map[string]HandlerList
+// ArgMap is a map of string to interface, it exists as a type alias for ease of use
 type ArgMap map[string]interface{}
 
+// HandlerFunc represents an event handler callback
 type HandlerFunc func(string, ArgMap)
 type Handler struct {
-	Func     HandlerFunc
-	Priority int
-	ID       int64
+	Func     HandlerFunc // The callback that this Handler refers to
+	Priority int         // The priority of this callback, lower is higher
+	ID       int64       // The ID of this callback
 }
 
+// Manager is an event system. It allows you to hook callbacks onto string based event names, and fire them later. Use
+// of Manager objects from multiple goroutines is permitted
 type Manager struct {
 	events Map
 	mutex  sync.Mutex
 }
 
+// HasEvent returns whether or not the given string exists as an event on this Manager
 func (m *Manager) HasEvent(name string) bool {
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
@@ -44,6 +52,8 @@ func (m *Manager) HasEvent(name string) bool {
 	return ok
 }
 
+// Attach adds an event and a callback to the Manager, the returned int64 is an ID for the attached callback, and can
+// be used to detach a callback later
 func (m *Manager) Attach(name string, f HandlerFunc, priority int) int64 {
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
@@ -56,6 +66,7 @@ func (m *Manager) Attach(name string, f HandlerFunc, priority int) int64 {
 	return id
 }
 
+// Detach removes a given ID from the event Manager. If the event is not found, Detach returns False
 func (m *Manager) Detach(id int64) bool {
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
@@ -80,6 +91,8 @@ loop:
 	return false
 }
 
+// Dispatch fires the event corresponding to the given string with the given ArgMap. If the event doesnt exist on this
+// Manager, Dispatch silently ignores the call
 func (m *Manager) Dispatch(name string, argMap ArgMap) {
 	m.mutex.Lock()
 	toIterate, ok := m.events[name]
