9cbb80af2ac08f9a845ae92d69d70475c765de12
A_D <A_D@snoonet.org>
2019-06-03 13:09:34 +0200

    added game's update from config method

--- internal/game/game.go
+++ internal/game/game.go
@@ -3,12 +3,12 @@ package game
 import (
 	"errors"
 	"fmt"
+	"path"
 	"strings"
 	"sync"
 	"time"
 
 	"git.ferricyanide.solutions/A_D/goGoGameBot/internal/config"
-	"git.ferricyanide.solutions/A_D/goGoGameBot/internal/interfaces"
 	"git.ferricyanide.solutions/A_D/goGoGameBot/internal/process"
 	"git.ferricyanide.solutions/A_D/goGoGameBot/pkg/log"
 	"git.ferricyanide.solutions/A_D/goGoGameBot/pkg/util"
@@ -24,45 +24,48 @@ func NewGame(conf config.Game, manager *Manager) (*Game, error) {
 	if conf.Name == "" {
 		return nil, errors.New("cannot have an empty game name")
 	}
-	cm, err := util.MakeColourMap(conf.ColourMap.ToMap())
-	if err != nil {
-		return nil, fmt.Errorf("could not build colour map for %s on %s: %s", conf.Name, manager, err)
-	}
-
-	fmts := conf.Chat.Formats
-	for k, v := range map[string]util.Format{"normal": fmts.Normal, "joinpart": fmts.JoinPart, "nick": fmts.Nick, "quit": fmts.Quit, "kick": fmts.Kick, "external": fmts.External} {
-		err := v.Compile(conf.Name+"_"+k, false)
+	/*	cm, err := util.MakeColourMap(conf.ColourMap.ToMap())
 		if err != nil {
-			return nil, fmt.Errorf("could not compile %q game chat format for %s", k, conf.Name)
+			return nil, fmt.Errorf("could not build colour map for %s on %s: %s", conf.Name, manager, err)
 		}
-	}
+
+		fmts := conf.Chat.Formats
+		for k, v := range map[string]util.Format{"normal": fmts.Normal, "joinpart": fmts.JoinPart, "nick": fmts.Nick, "quit": fmts.Quit, "kick": fmts.Kick, "external": fmts.External} {
+			err := v.Compile(conf.Name+"_"+k, false)
+			if err != nil {
+				return nil, fmt.Errorf("could not compile %q game chat format for %s", k, conf.Name)
+			}
+		}*/
 
 	g := &Game{
-		name:            conf.Name,
-		status:          normal,
-		autoRestart:     conf.AutoRestart,
-		autoStart:       conf.AutoStart,
-		stdinChan:       make(chan []byte),
-		controlChannels: channelPair{conf.StatusChannels.Admin, conf.StatusChannels.Msg},
-		chatBridge: chatBridge{
-			shouldBridge:  !conf.Chat.DontBridge,
-			dumpStderr:    conf.Chat.DumpStderr,
-			dumpStdout:    conf.Chat.DumpStdout,
-			allowForwards: !conf.Chat.DontAllowForwards,
-			stripMasks:    append(conf.Chat.StripMasks, manager.stripMasks...),
-			channels:      conf.Chat.BridgedChannels,
-			colourMap:     cm,
-			format: formatSet{
-				normal:   fmts.Normal,
-				joinPart: fmts.JoinPart,
-				nick:     fmts.Nick,
-				quit:     fmts.Quit,
-				kick:     fmts.Kick,
-				external: fmts.External,
-			},
-		},
+		name:   conf.Name,
+		status: normal,
+		/*		autoRestart:     conf.AutoRestart,
+				autoStart:       conf.AutoStart,
+				stdinChan:       make(chan []byte),
+				controlChannels: channelPair{conf.ControlChannels.Admin, conf.ControlChannels.Msg},
+				chatBridge: chatBridge{
+					shouldBridge:  !conf.Chat.DontBridge,
+					dumpStderr:    conf.Chat.DumpStderr,
+					dumpStdout:    conf.Chat.DumpStdout,
+					allowForwards: !conf.Chat.DontAllowForwards,
+					stripMasks:    append(conf.Chat.StripMasks, manager.stripMasks...),
+					channels:      conf.Chat.BridgedChannels,
+					colourMap:     cm,
+					format: formatSet{
+						normal:   fmts.Normal,
+						joinPart: fmts.JoinPart,
+						nick:     fmts.Nick,
+						quit:     fmts.Quit,
+						kick:     fmts.Kick,
+						external: fmts.External,
+					},
+				},*/
 		manager: manager,
-		Logger:  manager.Logger.Clone().SetPrefix(conf.Name),
+		Logger:  manager.Logger.Clone().SetPrefix("[" + conf.Name + "]"),
+	}
+	if err := g.UpdateFromConfig(conf); err != nil {
+		return nil, err
 	}
 
 	return g, nil
@@ -159,8 +162,63 @@ func (g *Game) runGame() (bool, error) {
 	return true, nil
 }
 
-func (g *Game) UpdateFromConfig(config config.Game) {
-	panic("implement me")
+// UpdateFromConfig updates the game object with the data from the config object. It is atomic--No changes will be applied
+// if any pre-processing errors, and that error will be returned
+func (g *Game) UpdateFromConfig(conf config.Game) error {
+	if conf.Name != g.GetName() {
+		g.Crit("attempt to reload game with a config who's name does not match ours! bailing out of reload")
+		return fmt.Errorf("invalid config name")
+	}
+	var wd string
+	if conf.WorkingDir == "" {
+		wd = path.Dir(conf.Path)
+		g.Logger.Infof("game %q's working directory inferred to %q from binary path %q", g.GetName(), wd, conf.Path)
+	}
+	if err := g.regexpManager.UpdateFromConf(conf.Regexps); err != nil {
+		return err
+	}
+
+	cm, err := util.MakeColourMap(conf.ColourMap.ToMap())
+	if err != nil {
+		return fmt.Errorf("could not create colour map for game %s from config: %s", g, err)
+	}
+	gName := g.GetName()
+	for _, err := range []error{
+		conf.Chat.Formats.Normal.Compile(gName+"_Normal", false),
+		conf.Chat.Formats.JoinPart.Compile(gName+"_JoinPart", false),
+		conf.Chat.Formats.Nick.Compile(gName+"_Nick", false),
+		conf.Chat.Formats.Quit.Compile(gName+"_Quit", false),
+		conf.Chat.Formats.Kick.Compile(gName+"_Kick", false),
+		conf.Chat.Formats.External.Compile(gName+"_External", false),
+	} {
+		if err != nil {
+			return fmt.Errorf("could not compile all formats for game %s (other formats may also have errored): %s", g, err)
+		}
+	}
+
+	g.process.UpdateCmd(conf.Path, strings.Split(conf.Args, " "), wd)
+	g.autoStart = conf.AutoStart
+	g.autoRestart = conf.AutoRestart
+	g.controlChannels.admin = conf.ControlChannels.Admin
+	g.controlChannels.msg = conf.ControlChannels.Msg
+
+	// Start of chat bridge configs
+	g.chatBridge.shouldBridge = !conf.Chat.DontBridge
+	g.chatBridge.dumpStdout = conf.Chat.DumpStdout
+	g.chatBridge.dumpStderr = conf.Chat.DumpStderr
+	g.chatBridge.allowForwards = !conf.Chat.DontAllowForwards
+	g.chatBridge.channels = conf.Chat.BridgedChannels
+	g.chatBridge.colourMap = cm
+	gf := &g.chatBridge.format
+	f := &conf.Chat.Formats
+	gf.normal = f.Normal
+	gf.joinPart = f.JoinPart
+	gf.nick = f.Nick
+	gf.quit = f.Quit
+	gf.kick = f.Kick
+	gf.external = f.External
+	g.Info("reload completed successfully")
+	return nil
 }
 
 func (g *Game) GetName() string {
