2d487f8d4e3422f3aca816f9dd18da36b62b8923
A_D <A_D@snoonet.org>
2019-06-07 15:15:12 +0200

    added test to ensure game.Game implements interfaces.Game

--- internal/game/game_test.go
+++ internal/game/game_test.go
@@ -1,1 +1,5 @@
 package game
+
+import "git.ferricyanide.solutions/A_D/goGoGameBot/internal/interfaces"
+
+var _ interfaces.Game = &Game{} // Make sure that Game is actually satisfying that interface
