bd9d610cd817868e408fd243893c8786bab1a8d7
A_D <A_D@snoonet.org>
2019-08-02 05:39:00 +0200

    Made sure to use IRC masks as the source for Hook*
    Previously just the nick was set as the Source of any of the Hook*
    methods that provided a source, this broke stuff on the other side when
    sending to sources.

--- internal/irc/irc_hooker.go
+++ internal/irc/irc_hooker.go
@@ -4,15 +4,16 @@ import (
 	"strings"
 
 	"git.ferricyanide.solutions/A_D/goGoGameBot/pkg/event"
+	"git.ferricyanide.solutions/A_D/goGoGameBot/pkg/util"
 )
 
 func (i *IRC) HookMessage(f func(source, channel, message string)) {
 	i.ParsedEvents.Attach("MSG", func(e event.Event) {
-		msg := e.(MessageEvent)
+		msg := e.(*MessageEvent)
 		if e.IsCancelled() || msg.IsNotice || !strings.HasPrefix(msg.Channel, "#") {
 			return
 		}
-		f(msg.Source.Nick, msg.Channel, msg.Message)
+		f(util.UserHost2Canonical(msg.Source), msg.Channel, msg.Message)
 
 	}, event.PriNorm)
 }
@@ -23,7 +24,7 @@ func (i *IRC) HookPrivateMessage(f func(source, channel, message string)) {
 		if e.IsCancelled() || msg.IsNotice || strings.HasPrefix(msg.Channel, "#") {
 			return
 		}
-		f(msg.Source.Nick, msg.Channel, msg.Message)
+		f(util.UserHost2Canonical(msg.Source), msg.Channel, msg.Message)
 
 	}, event.PriNorm)
 }
@@ -31,34 +32,34 @@ func (i *IRC) HookPrivateMessage(f func(source, channel, message string)) {
 func (i *IRC) HookJoin(f func(source, channel string)) {
 	i.ParsedEvents.Attach("JOIN", func(e event.Event) {
 		join := e.(*JoinEvent)
-		f(join.Source.Nick, join.Channel)
+		f(util.UserHost2Canonical(join.Source), join.Channel)
 	}, event.PriNorm)
 }
 
 func (i *IRC) HookPart(f func(source, channel, message string)) {
 	i.ParsedEvents.Attach("PART", func(e event.Event) {
 		part := e.(*PartEvent)
-		f(part.Source.Nick, part.Channel, part.Message)
+		f(util.UserHost2Canonical(part.Source), part.Channel, part.Message)
 	}, event.PriNorm)
 }
 
 func (i *IRC) HookQuit(f func(source, message string)) {
 	i.ParsedEvents.Attach("JOIN", func(e event.Event) {
 		quit := e.(*QuitEvent)
-		f(quit.Source.Nick, quit.Message)
+		f(util.UserHost2Canonical(quit.Source), quit.Message)
 	}, event.PriNorm)
 }
 
 func (i *IRC) HookKick(f func(source, channel, target, message string)) {
 	i.ParsedEvents.Attach("JOIN", func(e event.Event) {
 		kick := e.(*KickEvent)
-		f(kick.Source.Nick, kick.Channel, kick.KickedNick, kick.Message)
+		f(util.UserHost2Canonical(kick.Source), kick.Channel, kick.KickedNick, kick.Message)
 	}, event.PriNorm)
 }
 
 func (i *IRC) HookNick(f func(source, newNick string)) {
 	i.ParsedEvents.Attach("JOIN", func(e event.Event) {
 		nick := e.(*NickEvent)
-		f(nick.Source.Nick, nick.NewNick)
+		f(util.UserHost2Canonical(nick.Source), nick.NewNick)
 	}, event.PriNorm)
 }
