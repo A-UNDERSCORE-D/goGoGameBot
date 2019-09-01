b655c655fcdb91f6512159dc35dc76abd543bba8
aunderscored <aunderscored@gmail.com>
2019-09-01 22:39:40 +0200

    Clarified some documentation
    Specifically, added expected behaviour for some edge cases and fixed the
    incorrect title for the doc.go doc file

--- pkg/format/transformer/doc.go
+++ pkg/format/transformer/doc.go
@@ -1,4 +1,4 @@
-// Package formatTransformer implements a platform agnostic way of converting between platform specific formatting styles
+// Package transformer implements a platform agnostic way of converting between platform specific formatting styles
 //
 // In order to do this, an intermediate format is used internally and by all implementations of Transformer
 // The format is defined as follows
@@ -14,7 +14,8 @@
 //    c Colour
 //
 // Colour has a special definition, instead of simply $c, the sentinel and type is followed by six hex characters that
-// together indicate a colour.
+// together indicate a colour. It is expected that converters with a smaller colour space automatically condense any
+// unsupported colours into something within their colour space.
 //
-// Unintended uses of the sentinel must be escaped by using two dollar signs.
+// Unintended uses of the sentinel must be escaped by using two sentinels in a row.
 package transformer
--- pkg/format/transformer/transformer.go
+++ pkg/format/transformer/transformer.go
@@ -3,8 +3,11 @@ package transformer
 // Transformer refers to a string transformer. String Transformers convert messages from an intermediate format
 // to a protocol specific format
 type Transformer interface {
-	// Transform takes a string in the intermediate format and converts it to its specific format
+	// Transform takes a string in the intermediate format and converts it to its specific format.
+	// When the implementation of Transformer does not support a given format type, it can either eat it entirely, or
+	// use a pure ascii notation to indicate that it was there. For example: strike though could be replaced with ~~
 	Transform(in string) string
-	// MakeIntermediate takes a string in a Transformer specific format and converts it to the Intermediate  format
+	// MakeIntermediate takes a string in a Transformer specific format and converts it to the Intermediate format.
+	// Any existing sentinels in the string SHOULD be escaped
 	MakeIntermediate(in string) string
 }
