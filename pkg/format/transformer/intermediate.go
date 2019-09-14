9925a82c0f7166c5e86488f8f8778e45a851dd3f
aunderscored <aunderscored@gmail.com>
2019-09-14 17:07:33 +0200

    Added additional constants for Intermediate Format

--- pkg/format/transformer/intermediate.go
+++ pkg/format/transformer/intermediate.go
@@ -11,3 +11,25 @@ const (
 	Reset         = 'r'
 	Colour        = 'c'
 )
+
+// String representations of intermediate runes
+const (
+	SentinelString      = string(Sentinel)
+	BoldString          = string(Bold)
+	ItalicString        = string(Italic)
+	UnderlineString     = string(Underline)
+	StrikethroughString = string(Strikethrough)
+	ResetString         = string(Reset)
+	ColourString        = string(Colour)
+)
+
+// String representations of intermediate runes with a prefixed sentinel
+const (
+	SSentinelString      = SentinelString + SentinelString
+	SBoldString          = SentinelString + BoldString
+	SItalicString        = SentinelString + ItalicString
+	SUnderlineString     = SentinelString + UnderlineString
+	SStrikethroughString = SentinelString + StrikethroughString
+	SResetString         = SentinelString + ResetString
+	SColourString        = SentinelString + ColourString
+)
