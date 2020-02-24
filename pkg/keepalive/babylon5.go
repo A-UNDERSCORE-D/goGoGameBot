bb4d84fc738b80c4ac8ccc7f572554e1d83120ee
A_D <aunderscored@gmail.com>
2020-02-24 18:03:15 +0200

    Added more keepalive lines
    These are mostly just silly references

---
+++ pkg/keepalive/babylon5.go
@@ -0,0 +1,103 @@
+package keepalive
+
+var (
+	b5PilotMonologe = []string{
+		`I was there at the dawn of the third age of mankind.`,
+		`It began in the Earth year 2257,`,
+		` with the founding of the last of the Babylon stations, located deep in neutral space.`,
+		`It was a port of call for refugees, smugglers, businessmen, diplomats ...and travellers from a hundred worlds.`,
+		`Could be a dangerous place – but we accepted the risk, because Babylon 5 was our last, best hope for peace.`,
+		`Under the leadership of its final commander, Babylon 5 was a dream given form:`,
+		`a dream of a galaxy without war, when species from different worlds could live side by side in mutual respect.`,
+		`A dream that was in danger as never before, by the arrival of one man on a mission of destruction.`,
+		`Babylon 5 was the last of the Babylon stations. This is its story.... `,
+	}
+	b5MonologueS1 = []string{
+		`It was the dawn of the third age of mankind – ten years after the Earth-Minbari War.`,
+		`The Babylon Project was a dream, given form.`,
+		`Its goal:`,
+		`to prevent another war, by creating a place where humans and aliens can work out their differences peacefully.`,
+		`It's a port of call – home away from home – for diplomats, hustlers, entrepreneurs, and wanderers.`,
+		`Humans and aliens, wrapped in two million, five hundred thousand tons of spinning metal `,
+		`...all alone in the night.`,
+		`It can be a dangerous place, but it's our last best hope for peace.`,
+		`This is the story of the last of the Babylon stations.`,
+		`The year is 2258.`,
+		`The name of the place is Babylon 5.`,
+	}
+
+	b5MonologueS2 = []string{
+		`The Babylon Project was our last, best hope for peace.`,
+		`A self-contained world five miles long, located in neutral territory.`,
+		` A place of commerce and diplomacy for a quarter of a million humans and aliens.`,
+		`A shining beacon in space`,
+		`...all alone in the night.`,
+		`It was the dawn of the Third Age of Mankind – the year the Great War came upon us all.`,
+		`This is the story of the last of the Babylon stations. The year is 2259. The name of the place is Babylon 5.`,
+	}
+
+	b5S2FinalNotes = []string{
+		`It was the end of the Earth year 2259, and the war was upon us.`,
+		`As anticipated, a few days after the Earth-Centauri treaty was announced,`,
+		`The Centauri widened their war to include many of the Non-Aligned Worlds.`,
+		`And there was another war brewing closer to home.`,
+		`A personal one, whose cost would be higher than any of us could imagine.`,
+		`We came to this place because Babylon 5 was our last, best hope for peace.`,
+		`By the end of 2259, we knew that it had failed.`,
+		`But in so doing, it became something greater.`,
+		`As the war expanded, it became our last, best hope – for victory.`,
+		`Because sometimes, peace is another word for surrender.`,
+		`And because secrets – have a way of getting out.`,
+	}
+
+	b5MonologueS3 = []string{
+		`The Babylon Project was our last, best hope for peace.`,
+		`It failed.`,
+		`But in the year of the Shadow War, it became something greater:`,
+		`our last, best hope – for victory.`,
+		`The year is 2260.`,
+		`The place: Babylon 5.`,
+	}
+
+	b5S3FinalNotes = []string{
+		`It was the end of the Earth year 2260, and the war had paused, suddenly and unexpectedly.`,
+		`All around us, it was as if the universe were holding its breath ... waiting.`,
+		`All of life can be broken down into moments of transition or moments of revelation.`,
+		` This had the feeling of both.`,
+		`G'Quon wrote:`,
+		`"There is a greater darkness than the one we fight. It is the darkness of the soul that has lost its way."`,
+		`The war we fight is not against powers and principalities – it is against chaos and despair.`,
+		`Greater than the death of flesh is the death of hope, the death of dreams.`,
+		`Against this peril we can never surrender.`,
+		`The future is all around us, waiting in moments of transition, to be born in moments of revelation.`,
+		`No one knows the shape of that future, or where it will take us. We know only that it is always born in pain.`,
+	}
+
+	b5MonologueS4 = []string{
+		`It was the year of fire,`,
+		`the year of destruction,`,
+		`the year we took back what was ours.`,
+		`It was the year of rebirth,`,
+		`the year of great sadness,`,
+		`the year of pain,`,
+		`and the year of joy.`,
+		`It was a new age.`,
+		`It was the end of history.`,
+		`It was the year everything changed.`,
+		`The year is 2261.`,
+		`The place: Babylon 5.`,
+	}
+
+	b5S4FinalNotes = []string{
+		`It was the end of the Earth year 2261. It was the dawn of a new age for all of us.`,
+		`It was the end of one chapter, and the beginning of another.`,
+		`The next twenty years would see great changes, great joy, great sorrow.`,
+		`The telepath war.`,
+		`The Drakh war.`,
+		`The new Alliance would waver and crack.`,
+		`In the end, it would hold – because what is built endures.`,
+		`And what is loved endures.`,
+		`And Babylon 5... Babylon 5 endures.`,
+	}
+	// Would do the season 5 monologue too but it doesn't make sense on its own
+)
--- pkg/keepalive/next.go
+++ pkg/keepalive/next.go
@@ -1,9 +1,26 @@
+// Package keepalive contains various strings to use for keepalives.
+//
+// its essentially a bunch of references I added for fun, but it does serve the purpose of providing changing data
+// for use in keepalives
 package keepalive
 
 var curSet = 0
-var sets = [][]string{words, stayinAlive}
+var sets = [][]string{
+	words,
+	stayinAlive,
+	b5PilotMonologe,
+	b5MonologueS1,
+	b5MonologueS2,
+	b5S2FinalNotes,
+	b5MonologueS3,
+	b5S3FinalNotes,
+	b5MonologueS4,
+	b5S4FinalNotes,
+}
 var curIdx = 0
 
+// TODO: make this have a struct that stores the numbers, rather than doing it with globals. that way multiple things
+// TODO: can have their own response sets
 // Next returns the next string in the set, wrapping around if needed
 func Next() string {
 	if curIdx > len(sets[curSet])-1 {
